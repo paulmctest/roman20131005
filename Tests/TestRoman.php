517fb047f19c8fb34e76c55fde0ad09d6a7b8224
paul mcnally <paulmcnally000@gmail.com>
2013-10-12 20:29:40 +0100

    first basic tests

--- Tests/TestRoman.php
+++ Tests/TestRoman.php
@@ -4,14 +4,26 @@ require_once dirname(__FILE__) . '/../RomanBinary.php';
  
 class RomanBinaryTest extends PHPUnit_Framework_TestCase {
 
+    private $romanTokens       =   ['I'=>1, 'V'=>5, 'X'=>10, 'L'=>50, 'C'=>100, 'D'=>500, 'M'=>1000 ];
+    private $subtractionPairs  =   ['V'=>'I', 'X'=>'I', 'L'=>'X', 'C'=>'X', 'D'=>'C', 'M'=>'C' ];
+    private $illegalValues = [ 'IM','VM','LM','DM', 'ID','VD','LD','XD', 'IC','VC','LC', 'IC', 'VC', 'VX' ];
+
    function testCanCreateClass() {
-      $wrapper = new RomanBinary();
+      $roman = new RomanBinary();
    }
-/* 
-   function testItShouldWrapAnEmptyString() {
-      $rn = new RomanBinary();
-      $this->assertEquals('', $rn->roman(''));
+   function testItShouldReturnString() {
+      $roman = new RomanBinary();
+      $this->assertEquals('X', $roman->roman('X'));
    }
-*/ 
+   function testItShouldFailOnNonRomanChars() {
+      $roman = new RomanBinary();
+      $this->assertEquals('Your string must only contain Roman Numerals', $roman->roman('5'));
+   }
+   function testItShouldFailOnIllegalCharPatterns() {
+      $roman = new RomanBinary();
+      while (list($key, $value) = each($this->illegalValues)) {
+        $this->assertEquals('That is not a valid roman numeral', $roman->roman($value));
+      }
+   }
 }
 ?>
