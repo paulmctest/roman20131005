86809ca9de7b5323a3c23084b372ecb8e485283c
paul mcnally <paulmcnally000@gmail.com>
2013-10-12 23:38:21 +0100

    tests upto additions

--- RomanBinary.php
+++ RomanBinary.php
@@ -3,22 +3,32 @@
 
     public $romanTokens       =   ['I'=>1, 'V'=>5, 'X'=>10, 'L'=>50, 'C'=>100, 'D'=>500, 'M'=>1000 ];
     private $subtractionPairs  =   ['V'=>'I', 'X'=>'I', 'L'=>'X', 'C'=>'X', 'D'=>'C', 'M'=>'C' ];
-    public $illegalValues = [
-                    'IM','VM','LM','DM', 
-                    'ID','VD','LD','XD', 
-                    'IC','VC','LC',
-                    'IC', 'VC',
-                    'VX' 
-                ];
+    private $illegalValues      = [ 'IM','VM','LM','DM', 'ID','VD','LD','XD', 'IC','VC','LC', 'IC', 'VC', 'VX','DD','VV','LL' ];
 
-    function roman($string) {
-        if(!preg_match('/^[MDCLXVI]+$/', $string)){
+    function getBase10($input) {
+
+        $returnValue = '';
+        # Only Roman numerals
+        if(preg_match('/[^MDCLXVI]/', $input)){
             return 'Your string must only contain Roman Numerals';
         }
-        if(in_array($string, $this->illegalValues)){
+        # Is this a legal string?
+        if(in_array($input, $this->illegalValues)){
             return 'That is not a valid roman numeral';
         }
-        return $string;
+
+        //$returnValue = (int)$this->romanTokens[$input];
+        //return $returnValue;
+        if(strlen($input) == 1) return $this->romanTokens[$input];
+
+        for($i=0;$i<strlen($input);$i++){
+            
+            $currentValue += $this->romanTokens[$input[$i]];
+            $returnValue = $currentValue;
+        }
+/*
+*/
+        return $returnValue;
     } 
 }
 ?>
--- Tests/TestRoman.php
+++ Tests/TestRoman.php
@@ -4,25 +4,44 @@ require_once dirname(__FILE__) . '/../RomanBinary.php';
  
 class RomanBinaryTest extends PHPUnit_Framework_TestCase {
 
-    private $romanTokens       =   ['I'=>1, 'V'=>5, 'X'=>10, 'L'=>50, 'C'=>100, 'D'=>500, 'M'=>1000 ];
-    private $subtractionPairs  =   ['V'=>'I', 'X'=>'I', 'L'=>'X', 'C'=>'X', 'D'=>'C', 'M'=>'C' ];
-    private $illegalValues = [ 'IM','VM','LM','DM', 'ID','VD','LD','XD', 'IC','VC','LC', 'IC', 'VC', 'VX' ];
+    private $romanTokens        =   ['I'=>1, 'V'=>'5', 'X'=>'10', 'L'=>50, 'C'=>100, 'D'=>500, 'M'=>1000 ];
+    private $subtractionPairs   =   ['V'=>'I', 'X'=>'I', 'L'=>'X', 'C'=>'X', 'D'=>'C', 'M'=>'C' ];
+    private $illegalValues      = [ 'IM','VM','LM','DM', 'ID','VD','LD','XD', 
+                                     'IC','VC','LC', 'IC', 'VC', 'VX','DD','VV','LL' ];
+    //private $legalExamples      = [ 'M','LM','DM', 'ID','VD','LD','XD', 'IC','VC','LC', 'IC', 'VC', 'VX' ];
+    private $additionList       = [ 'MM'=>2000,'MMM'=>3000 ];
 
    function testCanCreateClass() {
-      $roman = new RomanBinary();
+      $rb = new RomanBinary();
    }
    function testItShouldReturnString() {
-      $roman = new RomanBinary();
-      $this->assertEquals('X', $roman->roman('X'));
+      $rb = new RomanBinary();
+      $this->assertEquals('10', $this->romanTokens['X']);
    }
    function testItShouldFailOnNonRomanChars() {
-      $roman = new RomanBinary();
-      $this->assertEquals('Your string must only contain Roman Numerals', $roman->roman('5'));
+      $rb = new RomanBinary();
+      $this->assertEquals('Your string must only contain Roman Numerals', $rb->getBase10('5'));
    }
    function testItShouldFailOnIllegalCharPatterns() {
-      $roman = new RomanBinary();
-      while (list($key, $value) = each($this->illegalValues)) {
-        $this->assertEquals('That is not a valid roman numeral', $roman->roman($value));
+      $rb = new RomanBinary();
+      while (list(, $value) = each($this->illegalValues)) {
+        $this->assertEquals('That is not a valid roman numeral', $rb->getBase10($value));
+      }
+   }
+   #Basic Single values test using romanTokes keys and values
+   function testSimpleSingles() {
+      $rb = new RomanBinary();
+      reset($this->romanTokens);
+      while (list($key, $value) = each($this->romanTokens)) {
+        $this->assertEquals($value, $rb->getBase10($key));
+      }
+   }
+
+   #Simple incrememntal addition test only
+   function testSimpleAddition(){
+      $rb = new RomanBinary();
+      while (list($key, $value) = each($this->additionList)) {
+        $this->assertEquals($value, $rb->getBase10($key));
       }
    }
 }
