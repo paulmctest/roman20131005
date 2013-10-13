729529cf47278e28605301a8db3996282e360eac
paul mcnally <paulmcnally000@gmail.com>
2013-10-13 13:09:57 +0100

    move illegal roman character check to a method

--- RomanBinary.php
+++ RomanBinary.php
@@ -8,7 +8,7 @@
 
     private $romanTokens            = ['I'=>1, 'V'=>5, 'X'=>10, 'L'=>50, 'C'=>100, 'D'=>500, 'M'=>1000 ];
     private $subtractionPairs       = ['IV', 'IX', 'XL', 'XC', 'CD', 'CM' ];
-    private $illegalRomanValues     = [ 'IM','VM','LM','DM', 'ID','VD','LD','XD', 'IC','VC','LC', 'IC', 'VC', 'VX','DD','VV','LL' ];
+    private $illegalRomanValues     = [ 'IM','VM','LM','DM', 'ID','VD','LD','XD', 'IC','VC','LC', 'IC', 'VC', 'VX','DD','VV','LL', 'IIII', 'VVVV', 'XXXX', 'LLLL', 'CCCC','DDDD','MMMM' ];
 
     function getBase10($input) {
 
@@ -18,8 +18,8 @@
             return 'Your string must only contain Roman Numerals';
         }
         # Is this a legal string?
-        if(in_array($input, $this->illegalRomanValues)){
-            return 'That is not a valid roman numeral';
+        if($this->checkValidRoman($input)){
+           return 'That is not a valid roman numeral';
         }
         # let's not loop for single values which are a simple lookup of existing arrays
         if(strlen($input) == 1) return $this->romanTokens[$input];
@@ -29,11 +29,6 @@
             $currentToken   = $input[$count]; 
             $nextToken      = $input[$count+1];
             $checkToken     = $currentToken.$nextToken;
-           /* 
-            if(in_array($currentToken.$nextToken, $this->illegalRomanValues)){
-                return 'That is not a valid roman numeral';
-            }
-            */
             //do subtraction
             if(in_array($checkToken, $this->subtractionPairs ))
             {
@@ -48,5 +43,13 @@
         }
         return $returnValue;
     } 
+
+    function checkValidRoman($string)
+    {
+        foreach($this->illegalRomanValues AS $testMatch){
+            if(preg_match("/$testMatch/", $string)) return 1;
+        }
+    }
+
 }
 ?>
--- Tests/TestRoman.php
+++ Tests/TestRoman.php
@@ -5,7 +5,7 @@ require_once dirname(__FILE__) . '/../RomanBinary.php';
 class RomanBinaryTest extends PHPUnit_Framework_TestCase {
 
     private $romanTokens             = ['I'=>1, 'V'=>5, 'X'=>10, 'L'=>50, 'C'=>100, 'D'=>500, 'M'=>1000 ];
-    private $illegalRomanValues           = [ 'IM','VM','LM','DM', 'ID','VD','LD','XD', 'IC','VC','LC', 'IC', 'VC', 'VX','DD','VV','LL' ];
+    private $illegalRomanPatterns    = [ 'IM','VM','LM','DM', 'ID','VD','LD','XD', 'IC','VC','LC', 'IC', 'VC', 'VX','DD','VV','LL', 'IIII', 'VVVV', 'XXXX', 'LLLL', 'CCCC','DDDD','MMMM' ];
     private $legalExamples           = [ 'M','LM','DM', 'ID','VD','LD','XD', 'IC','VC','LC', 'IC', 'VC', 'VX' ];
     private $validAdditionList       = [ 'MM'=>2000,'MMM'=>3000, 'III' => 3 ];
     private $validSubtractionList    = ['IV'=>4,'IX'=>9, 'XL' => 40, 'XC' => 90, 'CD'=>400, 'CM' => 900 ];
@@ -23,7 +23,7 @@ class RomanBinaryTest extends PHPUnit_Framework_TestCase {
    }
    function testItShouldFailOnNonRomanCharPatterns() {
       $rb = new RomanBinary();
-      while (list(, $value) = each($this->illegalRomanValues)) {
+      while (list(, $value) = each($this->illegalRomanPatterns)) {
         $this->assertEquals('That is not a valid roman numeral', $rb->getBase10($value));
       }
    }
