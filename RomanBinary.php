6eb06bb60a999b1eed01814b8df00df3c9f4f4a1
paul mcnally <paulmcnally000@gmail.com>
2013-10-13 12:09:33 +0100

    working with exception of IV, XL, CD

--- RomanBinary.php
+++ RomanBinary.php
@@ -1,33 +1,50 @@
 <?php
+/*
+ * A php class to convert convert between Roman Numerals and Arabic Numerals
+ * with checks for validity of input
+ *
+ */
     class RomanBinary {
 
-    public $romanTokens       =   ['I'=>1, 'V'=>5, 'X'=>10, 'L'=>50, 'C'=>100, 'D'=>500, 'M'=>1000 ];
-    private $subtractionPairs  =   ['V'=>'I', 'X'=>'I', 'L'=>'X', 'C'=>'X', 'D'=>'C', 'M'=>'C' ];
-    private $illegalValues      = [ 'IM','VM','LM','DM', 'ID','VD','LD','XD', 'IC','VC','LC', 'IC', 'VC', 'VX','DD','VV','LL' ];
+    private $romanTokens            = ['I'=>1, 'V'=>5, 'X'=>10, 'L'=>50, 'C'=>100, 'D'=>500, 'M'=>1000 ];
+    private $subtractionPairs       = ['I'=>'V', 'I'=>'X', 'X'=>'L', 'X' => 'C', 'C' => 'D', 'C' => 'M' ];
+    private $illegalRomanValues     = [ 'IM','VM','LM','DM', 'ID','VD','LD','XD', 'IC','VC','LC', 'IC', 'VC', 'VX','DD','VV','LL' ];
 
     function getBase10($input) {
 
-        $returnValue = '';
+        $returnValue = 0;
         # Only Roman numerals
         if(preg_match('/[^MDCLXVI]/', $input)){
             return 'Your string must only contain Roman Numerals';
         }
         # Is this a legal string?
-        if(in_array($input, $this->illegalValues)){
+        if(in_array($input, $this->illegalRomanValues)){
             return 'That is not a valid roman numeral';
         }
 
-        //$returnValue = (int)$this->romanTokens[$input];
-        //return $returnValue;
         if(strlen($input) == 1) return $this->romanTokens[$input];
 
-        for($i=0;$i<strlen($input);$i++){
+        $count = 0;
+        while($count<=strlen($input)){
+            $currentToken   = $input[$count]; 
+            $nextToken      = $input[$count+1];
             
-            $currentValue += $this->romanTokens[$input[$i]];
-            $returnValue = $currentValue;
+        print "$count : str length of '$input' = ".strlen($input)."\n";
+            if(in_array($currentToken.$nextToken, $this->illegalRomanValues)){
+                return 'That is not a valid roman numeral';
+            }
+            //do subtraction
+            if($this->subtractionPairs[$currentToken] == $nextToken )
+            {
+                $sum = (int)$this->romanTokens[$nextToken] - (int)$this->romanTokens[$currentToken];
+                $returnValue += $sum;
+                $count++;
+            }else{
+                $returnValue += (int)$this->romanTokens[$input[$count]];
+            }
+            #store currentToken for comparision in next iteration
+            $count++;
         }
-/*
-*/
         return $returnValue;
     } 
 }
--- Tests/TestRoman.php
+++ Tests/TestRoman.php
@@ -4,12 +4,11 @@ require_once dirname(__FILE__) . '/../RomanBinary.php';
  
 class RomanBinaryTest extends PHPUnit_Framework_TestCase {
 
-    private $romanTokens        =   ['I'=>1, 'V'=>'5', 'X'=>'10', 'L'=>50, 'C'=>100, 'D'=>500, 'M'=>1000 ];
-    private $subtractionPairs   =   ['V'=>'I', 'X'=>'I', 'L'=>'X', 'C'=>'X', 'D'=>'C', 'M'=>'C' ];
-    private $illegalValues      = [ 'IM','VM','LM','DM', 'ID','VD','LD','XD', 
-                                     'IC','VC','LC', 'IC', 'VC', 'VX','DD','VV','LL' ];
-    //private $legalExamples      = [ 'M','LM','DM', 'ID','VD','LD','XD', 'IC','VC','LC', 'IC', 'VC', 'VX' ];
-    private $additionList       = [ 'MM'=>2000,'MMM'=>3000 ];
+    private $romanTokens             = ['I'=>1, 'V'=>5, 'X'=>10, 'L'=>50, 'C'=>100, 'D'=>500, 'M'=>1000 ];
+    private $illegalValues           = [ 'IM','VM','LM','DM', 'ID','VD','LD','XD', 'IC','VC','LC', 'IC', 'VC', 'VX','DD','VV','LL' ];
+    private $legalExamples           = [ 'M','LM','DM', 'ID','VD','LD','XD', 'IC','VC','LC', 'IC', 'VC', 'VX' ];
+    private $validAdditionList       = [ 'III' => 3, 'CC' => 200, 2000,'MMM'=>3000 ];
+    private $validSubtractionList    = ['IV'=>-4,'IX'=>9, 'XL' => -40, 'XC' => 90, 'CD'=>-400, 'CM' => 900 ];
 
    function testCanCreateClass() {
       $rb = new RomanBinary();
@@ -22,25 +21,32 @@ class RomanBinaryTest extends PHPUnit_Framework_TestCase {
       $rb = new RomanBinary();
       $this->assertEquals('Your string must only contain Roman Numerals', $rb->getBase10('5'));
    }
-   function testItShouldFailOnIllegalCharPatterns() {
+   function testItShouldFailOnNonRomanCharPatterns() {
       $rb = new RomanBinary();
-      while (list(, $value) = each($this->illegalValues)) {
+      while (list(, $value) = each($this->illegalRomanValues)) {
         $this->assertEquals('That is not a valid roman numeral', $rb->getBase10($value));
       }
    }
    #Basic Single values test using romanTokes keys and values
-   function testSimpleSingles() {
+   function testRomanSimpleSingles() {
       $rb = new RomanBinary();
-      reset($this->romanTokens);
       while (list($key, $value) = each($this->romanTokens)) {
         $this->assertEquals($value, $rb->getBase10($key));
       }
    }
 
    #Simple incrememntal addition test only
-   function testSimpleAddition(){
+   function testRomanSimpleAddition(){
       $rb = new RomanBinary();
-      while (list($key, $value) = each($this->additionList)) {
+      while (list($key, $value) = each($this->validAdditionList)) {
+        $this->assertEquals($value, $rb->getBase10($key));
+      }
+   }
+
+   #Simple test of Roman values which subtract (IV, IX, XL)
+   function testRomanSimpleSubtraction(){
+      $rb = new RomanBinary();
+      while (list($key, $value) = each($this->validSubtractionList)) {
         $this->assertEquals($value, $rb->getBase10($key));
       }
    }
