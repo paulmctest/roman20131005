b87dbee104afce02f1e06cd13ac324619ad56bf9
paul mcnally <paulmcnally000@gmail.com>
2013-10-14 03:26:59 +0100

    uncommitted

--- index.php
+++ index.php
@@ -1,4 +1,40 @@
-<?PHP
+<!DOCTYPE html>  
+<html xmlns:fb="http://www.facebook.com/2008/fbml" xml:lang="en" lang="en">   
+<head>   
+    <title>Roman Numeral tool</title>  
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script type="text/javascript">
+    $( document ).ready(function() {
+        $("#convertForm").submit(function( event ) {
+            event.preventDefault();
 
-phpinfo();
-?>
+            var check = $('#inputRoman').val()? 'roman':'arabic';
+            var input = $('#inputRoman').val()? $('#inputRoman').val():$('#inputArabic').val();
+
+            var response = $.post( "process.php", 
+                   { 
+                    'check': check, 
+                    'input': input, 
+                   }) 
+            .done(function( data ) {
+                var aResp = JSON.parse(data);
+            });
+        });
+    });
+    </script>  
+</head>  
+<body>
+    <div id="submit-form">  
+        <form id="convertForm" method="post">  
+            <div class="formblock">  
+                <input type="text" name="inputRoman" id="inputRoman" value="" /> Make Numeric <br>
+                <input type="text" name="inputArabic" id="inputArabic" value="" /> Make Roman <br>
+            </div>  
+            <div id="showData"></div>                  
+                              
+            <button name="submit" id="formSubmit" type="submit" class="subbutton">Convert!</button>  
+        </form>             
+    </div>  
+                      
+    </div><!-- End #contact -->  
+</body>
